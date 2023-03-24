5d06940335377bc404b40bf6062d363958b87269
mareczek100_IntelliJ <mareczek100@wp.pl>
2023-03-24 19:37:21 +0100

    Person class modification 5

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -1,17 +1,26 @@
 package pl.zajavka.case2;
 
 import java.math.BigDecimal;
-record Person(String name, BigDecimal salary) {
+
+record Person(String name, Long age, BigDecimal salary) {
+
+    public String whatIsYourJob() {
+        return "Software developer!";
+    }
+
+    public String whatDoYouLike() {
+        return "Strawberries";
+    }
 
     public BigDecimal howMuchDoYouEarn() {
         return salary;
     }
 
-    public Boolean doYouLikePizza() {
+    public Boolean doYouHaveABike() {
         return true;
     }
 
-    public Boolean doYouHaveABike() {
+    public Boolean doYouLikePizza() {
         return true;
     }
 }
